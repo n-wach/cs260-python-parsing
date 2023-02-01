28eb8de0c10bae596b978699caafe0223b2a5a5d
n-wach <nmwachholz@gmail.com>
2023-01-31 20:16:23 -0800

    Copy over ir.py and parser.py

---
+++ src/ir.py
@@ -0,0 +1,326 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+import enum
+from typing import *
+
+
+class Type:
+    indirection: int = 0
+    base_type: str
+
+    def __init__(self, base_type, indirection=0):
+        self.base_type = base_type
+        self.indirection = indirection
+
+
+class StructField:
+    name: str
+    type: Type
+
+    def __init__(self, name, type_):
+        self.name = name
+        self.type = type_
+
+
+class Struct:
+    name: str
+    fields: List[StructField]
+
+    def __init__(self, name, fields):
+        self.name = name
+        self.fields = fields
+
+
+class Variable:
+    name: str
+    type: Type
+
+    def __init__(self, name, type_):
+        self.name = name
+        self.type = type_
+
+
+class Operand:
+    pass
+
+
+class VarOperand(Operand):
+    variable: Variable
+
+    def __init__(self, variable):
+        self.variable = variable
+
+
+class ConstIntOperand(Operand):
+    value: int
+
+    def __init__(self, value):
+        self.value = value
+
+
+class ConstFuncOperand(Operand):
+    function: str
+    type: Type
+
+    def __init__(self, function, type_):
+        self.function = function
+        self.type = type_
+
+
+class ConstNullPtrOperand(Operand):
+    type: Type
+
+    def __init__(self, type_):
+        self.type = type_
+
+
+class Program:
+    structs: List[Struct]
+    functions: List[Function]
+
+    def __init__(self, structs, functions):
+        self.structs = structs
+        self.functions = functions
+
+
+class Function:
+    name: str
+    return_type: Type
+    parameters: List[Variable]
+    basic_blocks: Dict[str, BasicBlock]
+    entry: BasicBlock
+
+    def __init__(self, name, return_type, parameters, basic_blocks):
+        self.name = name
+        self.return_type = return_type
+        self.parameters = parameters
+        self.basic_blocks = {}
+        for basic_block in basic_blocks:
+            self.basic_blocks[basic_block.label] = basic_block
+            basic_block.parent_function = self
+        self.entry = self.basic_blocks["entry"]
+        for basic_block in self.basic_blocks.values():
+            basic_block.resolve_labels()
+
+    def __repr__(self):
+        return f"<Function {self.name}>"
+
+
+class BasicBlock:
+    label: str
+    body: List[Instruction]
+    parent_function: Function
+    terminal: TerminalInst
+
+    def __init__(self, label, body):
+        self.label = label
+        self.body = body
+        self.terminal = body[-1]
+        for i, inst in enumerate(body):
+            inst.parent_block = self
+            inst.index = i
+
+    def resolve_labels(self):
+        if isinstance(self.terminal, JumpInst):
+            self.terminal.target = self.parent_function.basic_blocks[self.terminal.label]
+        if isinstance(self.terminal, BranchInst):
+            self.terminal.target_true = self.parent_function.basic_blocks[self.terminal.label_true]
+            self.terminal.target_false = self.parent_function.basic_blocks[self.terminal.label_false]
+
+    @property
+    def name(self):
+        return f"{self.parent_function.name}.{self.label}"
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __repr__(self):
+        return f"<BasicBlock {self.name}>"
+
+
+class Instruction:
+    index: int
+    parent_block: BasicBlock
+
+    def program_point(self):
+        return f"{self.parent_block.name}.{self.index}"
+
+
+class Aop(enum.Enum):
+    ADD = "add"
+    SUB = "sub"
+    MUL = "mul"
+    DIV = "div"
+
+
+class ArithInst(Instruction):
+    lhs: Variable
+    left_op: Operand
+    right_op: Operand
+    operation: Aop
+
+    def __init__(self, lhs, left_op, right_op, operation):
+        self.lhs = lhs
+        self.left_op = left_op
+        self.right_op = right_op
+        self.operation = operation
+
+
+class Rop(enum.Enum):
+    EQ = "eq"
+    NEQ = "neq"
+    LT = "lt"
+    GT = "gt"
+    LTE = "lte"
+    GTE = "gte"
+
+
+class CmpInst(Instruction):
+    lhs: Variable
+    left_op: Operand
+    right_op: Operand
+    operation: Rop
+
+    def __init__(self, lhs, left_op, right_op, operation):
+        self.lhs = lhs
+        self.left_op = left_op
+        self.right_op = right_op
+        self.operation = operation
+
+
+class PhiInst(Instruction):
+    lhs: Variable
+    ops: List[Operand]
+
+    def __init__(self, lhs, ops):
+        self.lhs = lhs
+        self.ops = ops
+
+
+class CopyInst(Instruction):
+    lhs: Variable
+    rhs: Operand
+
+    def __init__(self, lhs, rhs):
+        self.lhs = lhs
+        self.rhs = rhs
+
+
+class AllocInst(Instruction):
+    lhs: Variable
+
+    def __init__(self, lhs):
+        self.lhs = lhs
+
+    def abstract_execute(self, abstract_store):
+        raise NotImplementedError
+
+
+class AddrofInst(Instruction):
+    lhs: Variable
+    target: Operand
+
+    def __init__(self, lhs, target):
+        self.lhs = lhs
+        self.target = target
+
+
+class LoadInst(Instruction):
+    lhs: Variable
+    src_ptr: Operand
+
+    def __init__(self, lhs, src_ptr):
+        self.lhs = lhs
+        self.src_ptr = src_ptr
+
+
+class StoreInst(Instruction):
+    lhs: Variable
+    value: Operand
+
+    def __init__(self, lhs, value):
+        self.lhs = lhs
+        self.value = value
+
+
+class GepInst(Instruction):
+    lhs: Variable
+    src_ptr: Variable
+    index: Operand
+    field_name: str
+
+    def __init__(self, lhs, src_ptr, index, field_name):
+        self.lhs = lhs
+        self.src_ptr = src_ptr
+        self.index = index
+        self.field_name = field_name
+
+
+class SelectInst(Instruction):
+    lhs: Variable
+    condition: Operand
+    true_op: Operand
+    false_op: Operand
+
+    def __init__(self, lhs, condition, true_op, false_op):
+        self.lhs = lhs
+        self.condition = condition
+        self.true_op = true_op
+        self.false_op = false_op
+
+
+class CallInst(Instruction):
+    # direct function call "lhs = func_name(args)".
+    lhs: Variable
+    callee: str
+    args: List[Operand]
+
+    def __init__(self, lhs, callee, args):
+        self.lhs = lhs
+        self.callee = callee
+        self.args = args
+
+
+class ICallInst(Instruction):
+    # indirect function call "lhs = (*func_ptr)(args)".
+    lhs: Variable
+    function: Variable
+    args: List[Operand]
+
+    def __init__(self, lhs, function, args):
+        self.lhs = lhs
+        self.function = function
+        self.args = args
+
+
+class TerminalInst(Instruction):
+    pass
+
+
+class RetInst(TerminalInst):
+    retval: Operand
+
+    def __init__(self, retval):
+        self.retval = retval
+
+
+class JumpInst(TerminalInst):
+    label: str
+    target: BasicBlock
+
+    def __init__(self, label):
+        self.label = label
+
+
+class BranchInst(TerminalInst):
+    condition: Operand
+    label_true: str
+    label_false: str
+    target_true: BasicBlock
+    target_false: BasicBlock
+
+    def __init__(self, condition, label_true, label_false):
+        self.condition = condition
+        self.label_true = label_true
+        self.label_false = label_false
---
+++ src/parser.py
@@ -0,0 +1,269 @@
+from .ir import *
+import re
+
+
+class Parser:
+    remaining_text: str
+
+    def __init__(self, text: str):
+        self.remaining_text = text.strip()
+
+    def consume(self, expression):
+        result = re.match(f"^({expression})(.*)$", self.remaining_text, flags=re.DOTALL)
+        if result is None:
+            raise ValueError(f"Failed to consume '{expression}'")
+        token, remaining = result.groups()
+        self.remaining_text = remaining.lstrip()
+        return token.strip()
+
+    def lookahead_re(self, expression):
+        result = re.match(f"^({expression})(.*)$", self.remaining_text, flags=re.DOTALL)
+        return result is not None
+
+    def lookahead(self, value):
+        return self.remaining_text.startswith(value)
+
+    def parse_program(self) -> Program:
+        structs = self.parse_structs()
+        functions = self.parse_functions()
+        return Program(structs, functions)
+
+    def parse_structs(self) -> List[Struct]:
+        structs = []
+        while True:
+            if not self.lookahead("struct"):
+                break
+            struct = self.parse_struct()
+            structs.append(struct)
+        return structs
+
+    def parse_struct(self) -> Struct:
+        self.consume("struct")
+        name = self.parse_varname()
+        self.consume(r"\{")
+        fields = []
+        while not self.lookahead("}"):
+            field_name = self.parse_varname()
+            self.consume(":")
+            field_type = self.parse_type()
+            fields.append(StructField(field_name, field_type))
+        self.consume(r"\}")
+        return Struct(name, fields)
+
+    def parse_functions(self) -> List[Function]:
+        functions = []
+        while True:
+            if not self.lookahead("function"):
+                break
+            function = self.parse_function()
+            functions.append(function)
+        return functions
+
+    def parse_function(self) -> Function:
+        self.consume("function")
+        name = self.parse_varname()
+        self.consume(r"\(")
+        params = []
+        while not self.lookahead(")"):
+            param_name = self.parse_varname()
+            self.consume(":")
+            param_type = self.parse_type()
+            params.append(Variable(param_name, param_type))
+            if self.lookahead(","):
+                self.consume(",")
+        self.consume(r"\)")
+        self.consume("->")
+        return_type = self.parse_type()
+        self.consume(r"\{")
+        basic_blocks = []
+        while not self.lookahead("}"):
+            basic_blocks.append(self.parse_basic_block())
+        self.consume(r"\}")
+        return Function(name, return_type, params, basic_blocks)
+
+    def parse_basic_block(self) -> BasicBlock:
+        label = self.parse_label()
+        self.consume(":")
+        instructions = []
+        while True:
+            instruction = self.parse_instruction()
+            instructions.append(instruction)
+            if isinstance(instruction, TerminalInst):
+                break
+        return BasicBlock(label, instructions)
+
+    def parse_instruction(self) -> Instruction:
+        if self.lookahead("$"):
+            opcode = self.parse_opcode()
+
+            if opcode == "store":
+                value = self.parse_operand()
+                address = self.parse_operand()
+                return StoreInst(value, address)
+
+            if opcode == "ret":
+                operand = self.parse_operand()
+                return RetInst(operand)
+
+            if opcode == "jump":
+                label = self.parse_label()
+                return JumpInst(label)
+
+            if opcode == "branch":
+                condition = self.parse_operand()
+                true_label = self.parse_label()
+                false_label = self.parse_label()
+                return BranchInst(condition, true_label, false_label)
+
+            raise ValueError(f"Unknown instruction '{opcode}'")
+
+        lhs = self.parse_variable()
+        self.consume("=")
+        opcode = self.parse_opcode()
+        if opcode == "icall":
+            function = self.parse_operand()
+            self.consume(r"\(")
+            args = []
+            while not self.lookahead(")"):
+                args.append(self.parse_operand())
+                if self.lookahead(","):
+                    self.consume(",")
+            self.consume(r"\)")
+            return ICallInst(lhs, function, args)
+
+        if opcode == "call":
+            function = self.parse_varname()
+            self.consume(r"\(")
+            args = []
+            while not self.lookahead(")"):
+                args.append(self.parse_operand())
+                if self.lookahead(","):
+                    self.consume(",")
+            self.consume(r"\)")
+            return CallInst(lhs, function, args)
+
+        if opcode == "select":
+            condition = self.parse_operand()
+            true_op = self.parse_operand()
+            false_op = self.parse_operand()
+            return SelectInst(lhs, condition, true_op, false_op)
+
+        if opcode == "gep":
+            src = self.parse_operand()
+            index = self.parse_operand()
+            field_name = ""
+            if self.lookahead_re(r"[\w\.]+\s"):  # field name, no type
+                field_name = self.parse_varname()
+            return GepInst(lhs, src, index, field_name)
+
+        if opcode == "load":
+            src = self.parse_operand()
+            return LoadInst(lhs, src)
+
+        if opcode == "addrof":
+            rhs = self.parse_operand()
+            return AddrofInst(lhs, rhs)
+
+        if opcode == "alloc":
+            return AllocInst(lhs)
+
+        if opcode == "copy":
+            rhs = self.parse_operand()
+            return CopyInst(lhs, rhs)
+
+        if opcode == "phi":
+            self.consume(r"\(")
+            args = []
+            while not self.lookahead(")"):
+                args.append(self.parse_operand())
+                if self.lookahead(","):
+                    self.consume(",")
+            self.consume(r"\)")
+            return PhiInst(lhs, args)
+
+        if opcode == "cmp":
+            operation = self.parse_rop()
+            left_op = self.parse_operand()
+            right_op = self.parse_operand()
+            return CmpInst(lhs, left_op, right_op, operation)
+
+        if opcode == "arith":
+            operation = self.parse_aop()
+            left_op = self.parse_operand()
+            right_op = self.parse_operand()
+            return ArithInst(lhs, left_op, right_op, operation)
+
+        raise ValueError(f"Unknown instruction '{opcode}'")
+
+    def parse_rop(self) -> Rop:
+        rop = self.consume(r"\w+")
+        return Rop(rop)
+
+    def parse_aop(self) -> Aop:
+        aop = self.consume(r"\w+")
+        return Aop(aop)
+
+    def parse_operand(self) -> Operand:
+        if self.lookahead_re(r"-?\d+"):
+            return self.parse_const_int()
+        if self.lookahead("@nullptr"):
+            return self.parse_const_nullptr()
+        if self.lookahead("@"):
+            return self.parse_const_func()
+        return VarOperand(self.parse_variable())
+
+    def parse_opcode(self) -> str:
+        self.consume(r"\$")
+        return self.consume(r"\w+")
+
+    def parse_varname(self) -> str:
+        return self.consume(r"[\w\.]+")
+
+    def parse_label(self) -> str:
+        return self.consume(r"[\w\.]+")
+
+    def parse_type(self):
+        type_ = self.consume(r"[\w\[\]\*,]+[\s\(\),]")
+        if type_[-1] in "(),":
+            self.remaining_text = type_[-1] + self.remaining_text
+            type_ = type_[:-1]
+        indirection = 0
+        while type_.endswith("*"):
+            indirection += 1
+            type_ = type_[:-1]
+        return Type(type_, indirection)
+
+    def parse_const_int(self) -> ConstIntOperand:
+        value = self.consume(r"-?\d+")
+        return ConstIntOperand(int(value))
+
+    def parse_const_nullptr(self) -> ConstNullPtrOperand:
+        self.consume("@nullptr")
+        self.consume(":")
+        type_ = self.parse_type()
+        return ConstNullPtrOperand(type_)
+
+    def parse_const_func(self) -> ConstFuncOperand:
+        self.consume("@")
+        name = self.parse_varname()
+        self.consume(":")
+        type_ = self.parse_type()
+        return ConstFuncOperand(name, type_)
+
+    def parse_variable(self) -> Variable:
+        name = self.parse_varname()
+        self.consume(":")
+        type_ = self.parse_type()
+        return Variable(name, type_)
+
+
+if __name__ == "__main__":
+    import sys
+    import glob
+
+    for path in glob.glob(sys.argv[1]):
+        with open(path, "r") as f:
+            ir_text = f.read()
+
+        prog = Parser(ir_text).parse_program()
+        print(f"Parsed '{path}' without errors")
