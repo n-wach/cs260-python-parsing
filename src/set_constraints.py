bf57d60defcb65f8eaeecfc2ca3bc57a026d12f5
n-wach <nmwachholz@gmail.com>
2023-02-09 14:37:00 -0800

    Add set constraint parsing

---
+++ src/set_constraints.py
@@ -0,0 +1,161 @@
+from __future__ import annotations
+
+from typing import List, Union
+import re
+
+
+class SetVariable:
+    def __init__(self, name: str):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class Constructor:
+    def __init__(self, name: str, arity: int, contravariant_positions: List[int]):
+        self.name = name
+        self.arity = arity
+        self.contravariant_positions = contravariant_positions
+
+    def __str__(self):
+        return self.name
+
+
+class Proj:
+    def __init__(self, constructor: Constructor, var: SetVariable, index: int):
+        self.constructor = constructor
+        self.var = var
+        self.index = index
+
+    def __str__(self):
+        return f"proj({self.constructor}, {self.var}, {self.index})"
+
+
+class Call:
+    def __init__(self, constructor: Constructor, args: List[Union[SetVariable, Call, Proj]]):
+        self.constructor = constructor
+        self.args = args
+
+    def __str__(self):
+        return f"call({self.constructor}, {', '.join(map(str, self.args))})"
+
+
+class Constraint:
+    def __init__(self, left:  Union[SetVariable, Call, Proj], right:  Union[SetVariable, Call, Proj]):
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return f"{self.left} <= {self.right}"
+
+
+class SetConstraints:
+    def __init__(self, constructors: List[Constructor], set_variables: List[SetVariable], constraints: List[Constraint]):
+        self.constructors = constructors
+        self.set_variables = set_variables
+        self.constraints = constraints
+
+    @staticmethod
+    def parse(text):
+        """
+        INPUT GRAMMAR:
+        def constructor <name>, arity <int>, contravariant positions <int> <int> ...
+        def set variable <name>
+        <exp> <= <exp>
+
+        Constructor names and set variable names don't overlap (and don't include 'call' or 'proj')
+        <exp> is one of:
+        <set variable name>
+        call(<constructor name>, <exp>, <exp>, ...)
+        proj(<constructor name>, <set variable name>, <int>)
+        """
+        set_constraints = SetConstraints([], [], [])
+        for line in text.splitlines():
+            line = line.strip()
+            if len(line) == 0:
+                continue
+            if line.startswith("def constructor "):
+                set_constraints.add_parsed_constructor(line)
+            elif line.startswith("def set variable "):
+                set_constraints.add_parsed_set_variable(line)
+            else:
+                set_constraints.add_parsed_constraint(line)
+        return set_constraints
+
+    def add_parsed_constructor(self, line):
+        match = re.match(r"def constructor (\w+), arity (\d+), contravariant positions ?(.*)", line)
+        assert match is not None
+        name = match.group(1)
+        arity = int(match.group(2))
+        contravariant_positions = [int(x) for x in match.group(3).split()]
+        self.constructors.append(Constructor(name, arity, contravariant_positions))
+
+    def add_parsed_set_variable(self, line):
+        match = re.match(r"def set variable (\w+)", line)
+        assert match is not None
+        name = match.group(1)
+        self.set_variables.append(SetVariable(name))
+
+    def add_parsed_constraint(self, line):
+        match = re.match(r"(.*) <= (.*)", line)
+        assert match is not None
+        left = self.parse_expression(match.group(1))
+        right = self.parse_expression(match.group(2))
+        self.constraints.append(Constraint(left, right))
+
+    def parse_expression(self, text):
+        text = text.strip()
+        if text.startswith("call("):
+            return self.parse_call(text)
+        elif text.startswith("proj("):
+            return self.parse_proj(text)
+        else:
+            return self.get_set_variable(text)
+
+    def parse_call(self, text):
+        text = text.strip()
+        match = re.match(r"call\((\w+)(?:, (.*))?\)", text)
+        assert match is not None
+        constructor = self.get_constructor(match.group(1))
+        if match.group(2) is None:
+            return Call(constructor, [])
+        raw_args = match.group(2)
+        depth = 0
+        last = 0
+        args = []
+        for i, c in enumerate(raw_args):
+            if c == "(":
+                depth += 1
+            elif c == ")":
+                depth -= 1
+            elif c == "," and depth == 0:
+                arg = raw_args[last:i]
+                args.append(self.parse_expression(arg))
+                last = i + 1
+        args.append(self.parse_expression(raw_args[last:]))
+        return Call(constructor, args)
+
+    def parse_proj(self, text):
+        text = text.strip()
+        match = re.match(r"proj\((\w+), (\w+), (\d+)\)", text)
+        assert match is not None
+        constructor = self.get_constructor(match.group(1))
+        var = self.get_set_variable(match.group(2))
+        index = int(match.group(3))
+        return Proj(constructor, var, index)
+
+    def get_constructor(self, name):
+        name = name.strip()
+        for constructor in self.constructors:
+            if constructor.name == name:
+                return constructor
+        assert False
+
+    def get_set_variable(self, name):
+        name = name.strip()
+        for set_variable in self.set_variables:
+            if set_variable.name == name:
+                return set_variable
+        assert False
+
---
+++ test/test_set_constraint_parser.py
@@ -0,0 +1,72 @@
+from analysis.set_constraints import SetConstraints, Call, Proj, SetVariable
+
+
+def test_parser():
+    text = """
+    def constructor cons1, arity 2, contravariant positions 0 1
+    def constructor cons2, arity 1, contravariant positions
+    def constructor cons3, arity 0, contravariant positions
+    
+    def set variable var1
+    def set variable var2
+    
+    call(cons2, var1) <= var2
+    var1 <= proj(cons1, var2, 1)
+    call(cons3) <= var1
+    call(cons2, call(cons1, call(cons3), call(cons2, call(cons3)))) <= call(cons2, proj(cons2, var1, 0))
+    """
+    set_constraints = SetConstraints.parse(text)
+    assert len(set_constraints.constructors) == 3
+    assert len(set_constraints.set_variables) == 2
+    assert len(set_constraints.constraints) == 4
+
+    # call(cons2, var1) <= var2
+    assert isinstance(set_constraints.constraints[0].left, Call)
+    assert set_constraints.constraints[0].left.constructor is set_constraints.get_constructor("cons2")
+    assert set_constraints.constraints[0].left.args[0] is set_constraints.get_set_variable("var1")
+    assert isinstance(set_constraints.constraints[0].right, SetVariable)
+    assert set_constraints.constraints[0].right is set_constraints.get_set_variable("var2")
+
+    # var1 <= proj(cons1, var2, 1)
+    assert isinstance(set_constraints.constraints[1].left, SetVariable)
+    assert set_constraints.constraints[1].left is set_constraints.get_set_variable("var1")
+    assert isinstance(set_constraints.constraints[1].right, Proj)
+    assert set_constraints.constraints[1].right.constructor is set_constraints.get_constructor("cons1")
+    assert set_constraints.constraints[1].right.var is set_constraints.get_set_variable("var2")
+    assert set_constraints.constraints[1].right.index == 1
+
+    # call(cons3) <= var1
+    assert isinstance(set_constraints.constraints[2].left, Call)
+    assert set_constraints.constraints[2].left.constructor is set_constraints.get_constructor("cons3")
+    assert set_constraints.constraints[2].left.args == []
+    assert isinstance(set_constraints.constraints[2].right, SetVariable)
+    assert set_constraints.constraints[2].right is set_constraints.get_set_variable("var1")
+
+    # call(cons2, call(cons1, call(cons3), call(cons2, call(cons3)))) <= call(cons2, proj(cons2, var1, 0))
+    assert isinstance(set_constraints.constraints[3].left, Call)
+    assert set_constraints.constraints[3].left.constructor is set_constraints.get_constructor("cons2")
+    assert len(set_constraints.constraints[3].left.args) == 1
+    assert isinstance(set_constraints.constraints[3].left.args[0], Call)
+    assert set_constraints.constraints[3].left.args[0].constructor is set_constraints.get_constructor("cons1")
+    assert len(set_constraints.constraints[3].left.args[0].args) == 2
+    assert isinstance(set_constraints.constraints[3].left.args[0].args[0], Call)
+    assert set_constraints.constraints[3].left.args[0].args[0].constructor is set_constraints.get_constructor("cons3")
+    assert set_constraints.constraints[3].left.args[0].args[0].args == []
+    assert isinstance(set_constraints.constraints[3].left.args[0].args[1], Call)
+    assert set_constraints.constraints[3].left.args[0].args[1].constructor is set_constraints.get_constructor("cons2")
+    assert len(set_constraints.constraints[3].left.args[0].args[1].args) == 1
+    assert isinstance(set_constraints.constraints[3].left.args[0].args[1].args[0], Call)
+    assert set_constraints.constraints[3].left.args[0].args[1].args[0].constructor is set_constraints.get_constructor("cons3")
+    assert set_constraints.constraints[3].left.args[0].args[1].args[0].args == []
+    assert isinstance(set_constraints.constraints[3].right, Call)
+    assert set_constraints.constraints[3].right.constructor is set_constraints.get_constructor("cons2")
+    assert len(set_constraints.constraints[3].right.args) == 1
+    assert isinstance(set_constraints.constraints[3].right.args[0], Proj)
+    assert set_constraints.constraints[3].right.args[0].constructor is set_constraints.get_constructor("cons2")
+    assert set_constraints.constraints[3].right.args[0].var is set_constraints.get_set_variable("var1")
+    assert set_constraints.constraints[3].right.args[0].index == 0
+
+
+if __name__ == "__main__":
+    test_parser()
+
